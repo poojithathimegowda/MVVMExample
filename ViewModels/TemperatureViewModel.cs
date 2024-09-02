1bfde663faeaf8bebaa420fc0760a609733826e1
poojithathimegowda <poojithagowda98@gmail.com>
2024-09-02 17:31:19 +0200

    Temp convertor

---
+++ ViewModels/TemperatureViewModel.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace MVVMExample.ViewModels
+{
+    public class TemperatureViewModel : INotifyPropertyChanged
+    {
+        private double celsius;
+        private double fahrenheit;
+        private double kelvin;
+
+        public TemperatureViewModel()
+        {
+            ConvertCommand = new RelayCommand(ConvertTemperatures);
+        }
+
+        public double Celsius
+        {
+            get { return celsius; }
+            set { celsius = value; OnPropertyChanged("Celsius"); }
+        }
+
+        public double Fahrenheit
+        {
+            get { return fahrenheit; }
+            set { fahrenheit = value; OnPropertyChanged("Fahrenheit"); }
+        }
+
+        public double Kelvin
+        {
+            get { return kelvin; }
+            set { kelvin = value; OnPropertyChanged("Kelvin"); }
+        }
+
+        public ICommand ConvertCommand { get; private set; }
+
+        private void ConvertTemperatures(object obj)
+        {
+            Fahrenheit = (Celsius * 9.0 / 5.0) + 32;
+            Kelvin = Celsius + 273.15;
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected void OnPropertyChanged(string name)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
+        }
+    }
+
+    public class RelayCommand : ICommand
+    {
+        private Action<object> execute;
+
+        public RelayCommand(Action<object> executeAction)
+        {
+            execute = executeAction;
+        }
+
+        public event EventHandler CanExecuteChanged;
+
+        public bool CanExecute(object parameter)
+        {
+            return true;
+        }
+
+        public void Execute(object parameter)
+        {
+            execute(parameter);
+        }
+    }
+}
